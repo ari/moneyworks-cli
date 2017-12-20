25a150a20601ce076823211ded63105a9710d05e
Aristedes Maniatis <ari@ish.com.au>
2017-12-20 22:31:45 +1100

    First draft of a setup file to packge this project

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='moneywork-cli',
+    version='1.0.0',
+    description='A python API for Cognito Moneyworks accounting software',
+    long_description='A python API for Cognito Moneyworks accounting software',
+    url='http://github.com/ari/moneyworks-cli/',
+
+    author='Aristedes Maniatis',
+    author_email='ari@ish.com.au',
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='api moneyworks accounting',
+
+    py_modules=["moneyworks"],
+    install_requires=['requests']
+)
